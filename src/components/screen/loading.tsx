df0b28aee7e291838eacab0e2c1c565261dd02a8
tanahiro2010 <herentongkegu087@gmail.com>
2026-06-20 07:43:21 +0900

    feat: LoadingコンポーネントにisFullscreenプロパティを追加し、スタイルを動的に変更

--- src/components/screen/loading.tsx
+++ src/components/screen/loading.tsx
@@ -10,11 +10,15 @@ const members: Array<Member> = [
   { name: "田中博悠", href: "https://tanahiro2010.com" },
 ];
 
-const Loading = () => {
+type LoadingProps = {
+  isFullscreen?: boolean;
+}
+
+const Loading = ({ isFullscreen = true }: LoadingProps) => {
   const [member, _setMember] = useState<Member>(() => members[Math.floor(Math.random() * members.length)]);
 
   return (
-    <main className="loading">
+    <main className={`loading ${isFullscreen ? "fullscreen" : "inset"}`}>
       <div>
         <CircularProgress aria-label="Loading…"/>
         <span className="credit">Powered by <a href="https://unischool.jp">UniSchool</a> - <a href={member.href}>{member.name}</a></span>
