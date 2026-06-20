83f510a519a98f5f524462893de2c582e53a74c2
tanahiro2010 <herentongkegu087@gmail.com>
2026-06-20 21:29:27 +0900

    feat: Member型とメンバーリストを新規作成し、メンバー情報を管理する機能を追加

---
+++ src/constants/member.ts
@@ -0,0 +1,10 @@
+type Member = {
+  name: string;
+  href: string;
+}
+const members: Array<Member> = [
+  { name: "tanahiro2010", href: "https://tanahiro2010.com" },
+  { name: "田中博悠", href: "https://tanahiro2010.com" },
+];
+
+export { members, type Member };
