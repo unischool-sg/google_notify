c27f54680b64b1bc9e06563541e18f743e1bbbf2
tanahiro2010 <herentongkegu087@gmail.com>
2026-06-21 12:38:26 +0900

    feat: スタートアップ登録処理を追加

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,26 @@
+
+
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use auto_launch::AutoLaunch;
+fn setup_autostart() -> Result<(), Box<dyn std::error::Error>> {
+    let app = AutoLaunch::new(
+        "Google Notify",
+        std::env::current_exe()?
+            .to_str()
+            .unwrap(),
+        false,
+        &[] as &[&str],
+    );
+
+    if !app.is_enabled()? {
+        app.enable()?;
+    }
+
+    Ok(())
+}
 
 fn main() {
+    let _ = setup_autostart();
     google_notify_lib::run()
 }
