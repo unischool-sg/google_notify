7a6be7000de5b16b385395ed1000c8e05047b4ad
tanahiro2010 <herentongkegu087@gmail.com>
2026-06-21 09:58:30 +0900

    feat: 環境変数の読み込みをbuild.rsに追加

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,13 @@
 fn main() {
-    tauri_build::build()
+    dotenvy::from_filename("../.env").ok();
+    if let Ok(client_id) = std::env::var("GOOGLE_CLIENT_ID") {
+        println!("cargo:rustc-env=GOOGLE_CLIENT_ID={client_id}");
+    }
+
+    if let Ok(client_secret) = std::env::var("GOOGLE_CLIENT_SECRET") {
+        println!("cargo:rustc-env=GOOGLE_CLIENT_SECRET={client_secret}");
+    }
+
+    println!("cargo:rerun-if-changed=../.env");
+    tauri_build::build();
 }
